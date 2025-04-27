9d1ae074dcbb74183f8ca39a863dafc6aa69a406
kunalgoyal785 <kunalgoyal2006@gmail.com>
2025-04-27 22:34:50 +0530

    assignment 1.c

--- assignment 1.c
+++ assignment 1.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-
-// Function to find the factorial of a number
 int factorial(int n) {
     if (n == 0 || n == 1)
         return 1;
     return n * factorial(n - 1);
 }
-
-// Function to check if a number is prime
 int is_prime(int num) {
     if (num < 2)
         return 0;
@@ -17,8 +13,6 @@ int is_prime(int num) {
     }
     return 1;
 }
-
-// Function to find the greatest common divisor (GCD) of two numbers
 int gcd(int a, int b) {
     if (b == 0)
         return a;
@@ -27,13 +21,9 @@ int gcd(int a, int b) {
 
 int main() {
     int num;
-
-    // Problem 1: Finding Factorial
     printf("Enter a number to find its factorial: ");
     scanf("%d", &num);
     printf("Factorial of %d is %d\n", num, factorial(num));
-
-    // Problem 2: Checking if a number is prime
     printf("\nEnter a number to check if it's prime: ");
     scanf("%d", &num);
     if (is_prime(num))
@@ -41,7 +31,6 @@ int main() {
     else
         printf("%d is not a prime number.\n", num);
 
-    // Problem 3: Finding GCD of two numbers
     int a, b;
     printf("\nEnter two numbers to find their GCD: ");
     scanf("%d %d", &a, &b);
